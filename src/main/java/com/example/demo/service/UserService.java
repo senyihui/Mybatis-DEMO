a316519568312f9896b7da6adb5c7f3184d14b27
Syh <986245600@qq.com>
2021-09-24 17:07:09 +0800

    SpringBoot xml配置bean

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -7,11 +7,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
+//@Service
+//@AllArgsConstructor
 public class UserService {
-
-//    @Autowired
-//    private UserMapper userMapper;
 
     @Autowired
     private UserDao userDao;
